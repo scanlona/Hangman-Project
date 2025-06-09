8911932598ccc2cef1c5889bac251a3016a8cb34
scanlona <59579777+scanlona@users.noreply.github.com>
2025-06-09 13:56:32 -0700

    Building Out Core I/O, Establishing String Arrays
    Starting building the project, accepting user input continually until you break for exit, and it stores the inputs and outputs them.

---
+++ hangman.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+char userInput[100];
+char userGuesses[100];
+
+int main() {
+    printf("Starting program...\n");
+    while (strcmp(userInput, "exit") != 0) {
+        printf("\nInput a letter to guess:\n");
+        printf("(Enter 'exit' to exit)\n\n");
+        scanf("%s", userInput);
+        if (strcmp(userInput, "exit") == 0) {
+            break;
+        }
+        strcat(userGuesses, userInput);
+        printf("You input: %s\n", userGuesses);
+    }
+    printf("Program complete.\n");
+    return 0;
+}
